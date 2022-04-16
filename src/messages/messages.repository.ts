418231ed332ad7a1c9a3a3cfea3e37c95db812e8
hashibadaiki <higemeganeking@gmail.com>
2022-04-16 11:32:33 +0900

    add: repository / service

---
+++ src/messages/messages.repository.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { promises } from 'fs';
+
+const { readFile, writeFile } = promises;
+
+// 本来はDBとのやりとりをするところだけど、今回はfileの読み書きで対応している
+
+@Injectable()
+export class MessagesRepository {
+  async findOne(id: string) {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+
+    return messages[id];
+  }
+
+  async findAll() {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+
+    return messages;
+  }
+
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+
+    const id = Math.floor(Math.random() * 999);
+
+    messages[id] = { id, content };
+
+    await writeFile('messages.json', JSON.stringify(messages));
+  }
+}
