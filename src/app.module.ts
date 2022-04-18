7f4cd47f1a1168e5bbb75754b00077ebee4e9951
hashibadaiki <higemeganeking@gmail.com>
2022-04-18 23:16:43 +0900

    不要なコードとファイルを削除した

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { User } from './users/user.entity';
 import { UserModule } from './users/user.module';
 
@@ -15,7 +13,5 @@ import { UserModule } from './users/user.module';
     }),
     UserModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
